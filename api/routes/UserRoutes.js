bf89ee6a88a610f581a84aa9df953792ab30e9a6
Hugo HANOCQ <hhanocq@gmail.com>
2024-08-21 17:23:39 +0200

    add swagger, route with autorization not working

--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -1,11 +1,11 @@
+const express = require("express");
 const { hashPassword } = require("../middlewares/hashPassword");
 const { verifyToken } = require("../middlewares/jwtMiddleware");
 const { register, login, handleInvitation, getAll, getMySanta } = require("../controllers/UserController");
-const express = require("express");
 
-/* User Routes */
 const userRouter = express.Router();
 
+// User routes
 userRouter.post("/register", hashPassword, register);
 userRouter.post("/login", login);
 userRouter.post("/group/:groupId", verifyToken, handleInvitation);
--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { userRouter } = require("./api/routes/UserRoutes");
 const { groupRouter } = require("./api/routes/GroupRoutes");
+const cors = require("cors");
+const YAML = require("yamljs");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = YAML.load("./doc/swagger.yaml");
 
 const { PORT = 3000, DB_PORT = "127.0.0.1" } = process.env;
 
 const app = express();
+app.use(cors());
 
 mongoose
   .connect(`mongodb://${DB_PORT}:27017/secret-santa`)
@@ -16,9 +21,11 @@ mongoose
 app.use(express.json());
 
 // Mounting the routers
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use("/users", userRouter);
 app.use("/group", groupRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server is running: http://localhost:${PORT}`);
+  console.log(`Server is running: http://localhost:${PORT}/api-docs`);
 });
